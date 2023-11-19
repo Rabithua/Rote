71b1bb58e267abef0479009db2cac496b8aaf286
Rabithua <rabithua@gmail.com>
2023-11-19 19:37:41 +0800

    update

--- script.ts
+++ script.ts
@@ -3,7 +3,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+  const users = await prisma.user.findMany()
+  console.log(users)
 }
 
 main()
