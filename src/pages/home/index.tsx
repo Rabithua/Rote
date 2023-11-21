94cd28492bc235744cdc48e26805ace7f2e06523
Rabithua <rabithua@gmail.com>
2023-11-21 17:13:04 +0800

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,21 @@
 import { Editor } from "novel";
-
+import { useState } from "react";
 
 function Home() {
-    return (
-      <div className=" w-full flex justify-center">
-        <Editor className=" mt-6 relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg" onUpdate={(e) => {
-          console.log(e)
-        }} />
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+  const [htmlContent, setHtmlContent] = useState<string | undefined>("");
+  return (
+    <div className=" w-full flex flex-col items-center">
+      <h1>123</h1>
+      <Editor
+        className=" mt-6 relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg"
+        onUpdate={(e) => {
+          setHtmlContent(e?.getHTML());
+        }}
+      />
+      <article className=" prose lg:prose-xl" dangerouslySetInnerHTML={{ __html: htmlContent ? htmlContent : "" }}>
+      </article>
+    </div>
+  );
+}
+
+export default Home;
