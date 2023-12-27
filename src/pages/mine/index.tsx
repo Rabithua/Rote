36b40e1bf17f8ef770795e8790d553176571be4a
Rabithua <rabithua@gmail.com>
2023-12-27 13:58:32 +0800

    update

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,34 +1,8 @@
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
-import {
-  Icon,
-  IconProps,
-  StoryBook,
-  useControls,
-  useCreateStore,
-} from "@lobehub/ui";
-import { Settings } from "lucide-react";
 
 function Mine() {
   const { t } = useTranslation("translation", { keyPrefix: "pages.mine" });
-  const store = useCreateStore();
-  const size: IconProps["size"] | any = useControls(
-    {
-      fontSize: {
-        max: 100,
-        min: 8,
-        step: 4,
-        value: 28,
-      },
-      strokeWidth: {
-        max: 2,
-        min: 1,
-        step: 0.5,
-        value: 2,
-      },
-    },
-    { store }
-  );
   const icons = [
     {
       svg: (
@@ -231,9 +205,6 @@ function Mine() {
               );
             })}
           </div>
-          <StoryBook levaStore={store}>
-            <Icon icon={Settings} size={size} />
-          </StoryBook>
         </div>
       </div>
     </div>
