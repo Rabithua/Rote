352d962481199f1ff98cbbea2fd59000354fd05c
Rabithua <1289770378@qq.com>
2024-04-01 16:04:30 +0800

    ✨ feat: 添加了用户描述和背景图字段

--- frontend/src/pages/mine/profile/index.tsx
+++ frontend/src/pages/mine/profile/index.tsx
@@ -21,33 +21,37 @@ function ProfilePage() {
   return (
     <div>
       <div className=" w-full max-h-80 overflow-hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 100 40"
-          fill="none"
-        >
-          <g clipPath="url(#clip0_2027_1853)">
-            <rect width="100" height="40" fill="white" />
-            <rect
-              x="61"
-              y="23"
-              width="28.7687"
-              height="28.7687"
-              rx="14.3844"
-              fill="white"
-            />
-            <path
-              d="M91 28.3739V30.4495C91 31.1582 90.8497 31.86 90.5575 32.5147C90.2654 33.1695 89.8372 33.7644 89.2974 34.2655C88.7576 34.7666 88.1168 35.1641 87.4115 35.4353C86.7063 35.7065 85.9504 35.8461 85.187 35.8461H82.9512V39.9973H81.1626V34.1856L81.1796 33.3554C81.2922 32 81.9517 30.734 83.0263 29.8104C84.1008 28.8869 85.5113 28.3739 86.9756 28.3739H91ZM76.691 25.053C78.0051 25.0531 79.2858 25.4368 80.352 26.1499C81.4182 26.863 82.2158 27.8694 82.6319 29.0265C81.9476 29.5649 81.3869 30.2256 80.9833 30.9693C80.5797 31.713 80.3414 32.5244 80.2826 33.3554H79.374C77.7137 33.3554 76.1213 32.7431 74.9473 31.6532C73.7733 30.5633 73.1138 29.0851 73.1138 27.5437V25.053H76.691Z"
-              fill="#07C160"
-              fillOpacity="0.06"
-            />
-          </g>
-          <defs>
-            <clipPath id="clip0_2027_1853">
+        {profile?.cover ? (
+          <img src={profile?.cover} alt="" />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 40"
+            fill="none"
+          >
+            <g clipPath="url(#clip0_2027_1853)">
               <rect width="100" height="40" fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
+              <rect
+                x="61"
+                y="23"
+                width="28.7687"
+                height="28.7687"
+                rx="14.3844"
+                fill="white"
+              />
+              <path
+                d="M91 28.3739V30.4495C91 31.1582 90.8497 31.86 90.5575 32.5147C90.2654 33.1695 89.8372 33.7644 89.2974 34.2655C88.7576 34.7666 88.1168 35.1641 87.4115 35.4353C86.7063 35.7065 85.9504 35.8461 85.187 35.8461H82.9512V39.9973H81.1626V34.1856L81.1796 33.3554C81.2922 32 81.9517 30.734 83.0263 29.8104C84.1008 28.8869 85.5113 28.3739 86.9756 28.3739H91ZM76.691 25.053C78.0051 25.0531 79.2858 25.4368 80.352 26.1499C81.4182 26.863 82.2158 27.8694 82.6319 29.0265C81.9476 29.5649 81.3869 30.2256 80.9833 30.9693C80.5797 31.713 80.3414 32.5244 80.2826 33.3554H79.374C77.7137 33.3554 76.1213 32.7431 74.9473 31.6532C73.7733 30.5633 73.1138 29.0851 73.1138 27.5437V25.053H76.691Z"
+                fill="#07C160"
+                fillOpacity="0.06"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_2027_1853">
+                <rect width="100" height="40" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        )}
       </div>
       <div className=" flex mx-4 h-16">
         <Avatar
@@ -64,7 +68,9 @@ function ProfilePage() {
       <div className=" flex flex-col mx-4 gap-1">
         <div className=" text-2xl font-semibold">{profile?.nickname}</div>
         <div className=" text-base text-gray-500">@{profile?.username}</div>
-        <div className=" text-base ">这个人很懒，还没留下任何简介...</div>
+        <div className=" text-base ">
+          {profile?.description || "这个人很懒，还没留下任何简介..."}
+        </div>
         <div className=" text-base text-gray-500">{`注册时间：${moment
           .utc(profile?.createdAt)
           .format("YYYY/MM/DD HH:mm:ss")}`}</div>
