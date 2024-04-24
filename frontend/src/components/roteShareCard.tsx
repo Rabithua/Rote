f620c9e8856189f9cf988a8e2571f25f7ec985a6
Rabithua <1289770378@qq.com>
2024-04-24 15:12:06 +0800

    🐞 fix: 样式调整

--- frontend/src/components/roteShareCard.tsx
+++ frontend/src/components/roteShareCard.tsx
@@ -12,10 +12,11 @@ function RoteShareCard({ rote }: any) {
 
       // 配置 html2canvas 选项
       const options: any = {
-        scale: 3, // 设置缩放比例为 2
+        scale: 1080 / width, // 设置缩放比例为 2
         width: width,
         height: height,
         useCORS: true, // 允许跨域图像
+        backgroundColor: null,
       };
 
       html2canvas(element, options).then((canvas: HTMLCanvasElement) => {
@@ -48,7 +49,7 @@ function RoteShareCard({ rote }: any) {
         <div className=" font-extrabold text-5xl text-gray-200 mb-[-10px]">
           “
         </div>
-        <div className=" text-base text-gray-800 font-semibold break-words whitespace-pre-line font-serif">
+        <div className=" text-base text-gray-700 font-semibold break-words whitespace-pre-line font-serif">
           {rote.content}
         </div>
         <div className=" flex flex-wrap gap-2 items-center">
@@ -62,7 +63,10 @@ function RoteShareCard({ rote }: any) {
         </div>
         <Divider />
         <div className=" w-full flex flex-wrap">
-          <span className=" font-semibold text-gray-800">{rote.author.nickname}</span>
+          {/* <img className=" w-6 h-6 mr-2 rounded-full" src={rote.author.avatar} alt="" /> */}
+          <span className=" font-semibold text-gray-800">
+            {rote.author.nickname}
+          </span>
           <span className=" text-nowrap ml-auto font-normal text-gray-500">
             来自 Rote.ink/{rote.author.username}
           </span>
