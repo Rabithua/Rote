7e76835d98389081561d4c274c58c733c6d4953d
Rabithua <1289770378@qq.com>
2024-06-25 16:42:59 +0800

    🐞 fix: 返回顶部按钮层级，heatmap位置

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -8,16 +8,10 @@ import Heatmap from "@/components/d3/heatmap";
 import TagMap from "@/components/tagMap";
 import { useProfile } from "@/state/profile";
 import RandomRote from "@/components/randomRote";
+import GoTop from "@/components/goTop";
 
 function RotePage() {
   const profile = useProfile();
-  function goTop() {
-    const containers = document.getElementsByClassName("scrollContainer");
-    if (containers.length > 0) {
-      const container = containers[0]; // 获取第一个匹配的元素
-      container.scrollTop = 0; // 将该容器滚动到顶部
-    }
-  }
   return (
     <div className=" flex w-full h-dvh">
       <div
@@ -176,12 +170,7 @@ function RotePage() {
           }}
         />
 
-        <div
-          className=" animate-show duration-300 fixed self-end right-8 bottom-8 bg-black w-fit py-2 px-4 rounded-md text-white cursor-pointer hover:text-white"
-          onClick={goTop}
-        >
-          <UpOutlined />
-        </div>
+        <GoTop scrollContainerName="scrollContainer" />
       </div>
       <div className=" gap-4 hidden md:flex flex-col w-72 shrink-0 scrollContainer scroll-smooth overscroll-contain noScrollBar h-dvh overflow-y-visible overflow-x-hidden relative p-4">
         <div className=" flex gap-2 bg-white text-lg font-semibold">
