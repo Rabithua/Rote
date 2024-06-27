232d914e8446a54aed9b1154b106dcbd0d5160e4
Rabithua <1289770378@qq.com>
2024-06-27 12:09:07 +0800

    🐞 fix: 对webpush的无配置情况进行处理

--- backend/utils/webpush.ts
+++ backend/utils/webpush.ts
@@ -6,10 +6,14 @@ const apiKeys = {
   privateKey: process.env.VAPID_PRIVATE_KEY as string,
 };
 
-webpush.setVapidDetails(
-  "mailto:rabit.hua@gmail.com",
-  apiKeys.publicKey,
-  apiKeys.privateKey
-);
+if (!apiKeys.publicKey || !apiKeys.privateKey) {
+  console.info("VAPID keys are not provided");
+} else {
+  webpush.setVapidDetails(
+    "mailto:rabit.hua@gmail.com",
+    apiKeys.publicKey,
+    apiKeys.privateKey
+  );
+}
 
-export default webpush;
+export default apiKeys.publicKey && apiKeys.privateKey ? webpush : null;
