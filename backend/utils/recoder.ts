ce54d843e1fc28a3bf2ca284663db156f3a8cf96
Rabithua <1289770378@qq.com>
2024-12-11 21:21:08 +0800

    refactor: make code more elegent

--- backend/utils/recoder.ts
+++ backend/utils/recoder.ts
@@ -1,14 +1,18 @@
 import moment from "moment";
+import { Request, Response, NextFunction } from "express";
 
 // Request middleware, record IP and time
-const recoderIpAndTime = function (req: any, res: any, next: any) {
-  const ipAddress = req.ip;
-  console.log(
-    `${moment().format("YYYY/MM/DD HH:mm:ss")} ${ipAddress} ${req.method} ${
-      req.path
-    }`
-  );
+export const recorderIpAndTime = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const ipAddress =
+    req.headers["x-forwarded-for"] || req.headers["x-real-ip"] || req.ip;
+
+  const logMessage = `[${moment().format(
+    "YYYY/MM/DD HH:mm:ss"
+  )}] IP: ${ipAddress} | Method: ${req.method} | Path: ${req.path}`;
+  console.log(logMessage);
   next();
 };
-
-export default recoderIpAndTime;
