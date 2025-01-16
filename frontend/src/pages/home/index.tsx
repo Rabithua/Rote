46dce3176cf30504ca1868171843bb908f4a4d8e
Rabithua <1289770378@qq.com>
2025-01-16 15:08:14 +0800

    Update index.tsx

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -13,7 +13,7 @@ import { useTranslation } from "react-i18next";
 const Statistics = ({ t }: { t: (key: string) => string }) => {
   return (
     <div className=" w-72 shrink-0 relative hidden md:block">
-      <div className="p-4  w-72 fixed top-0 overflow-y-scroll noScrollBar hidden md:block">
+      <div className="p-4 h-dvh w-72 fixed top-0 overflow-y-scroll noScrollBar hidden md:block">
         <div className="gap-4 w-full sticky top-0 flex flex-col">
           <div className="flex gap-2 text-lg font-semibold">
             <BarChartOutlined />
