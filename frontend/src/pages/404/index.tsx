3911cfe099f92b39aacc37c252a874a845b66b9d
Rabithua <1289770378@qq.com>
2025-04-02 22:56:44 +0800

    feat: 更新 404 页面样式，调整文本颜色以提高可读性

--- frontend/src/pages/404/index.tsx
+++ frontend/src/pages/404/index.tsx
@@ -7,13 +7,13 @@ export default function ErrorPage() {
     <>
       <main className="h-dvh place-items-center bg-bgLight dark:bg-bgDark px-6 flex justify-center items-center">
         <div className="flex flex-col gap-5">
-          <p className="text-[100px] lg:text-[200px] font-semibold font-mono bg-black text-transparent bg-clip-text">
+          <p className="text-[100px] lg:text-[200px] text-black dark:text-white font-semibold font-mono bg-black text-transparent bg-clip-text">
             404
           </p>
-          <h1 className="text-base lg:text-2xl font-bold tracking-tight text-gray-900">
+          <h1 className="text-base lg:text-2xl font-bold tracking-tight text-black/90 dark:text-white/90">
             {t("pageNotFound")}
           </h1>
-          <p className="text-base leading-7 text-gray-600">
+          <p className="text-base leading-7 text-black/90 dark:text-white/90">
             {t("pageNotFoundDesc")}
           </p>
         </div>
